0c179bdd2c5447134d9b0b5adf44d32b13e0b0ed
Aldi Destaryana <aldidestaryana96@gmail.com>
2021-06-27 00:03:47 +0700

    option belom

---
+++ helpers/util.js
@@ -0,0 +1,9 @@
+module.exports = {
+    isLoggedIn: (req, res, next) => {
+        if (req.session.users) {
+            next()
+        } else {
+            res.redirect('/login')
+        }
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require("express");
 var router = express.Router();
-var moment = require("moment");
+// var moment = require("moment");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const helpers = require('../helpers/util')
 
 module.exports = function (db) {
   router.get("/login", (req, res, next) => {
@@ -10,36 +11,61 @@ module.exports = function (db) {
   });
 
   router.post("/login", (req, res, next) => {
-    let sql = `select * from users where email = '${req.body.email}' and password = '${req.body.password}'`;
-    db.query(sql, (err, row) => {
+    // let sql =  and password = '${req.body.password}'`;
+    db.query('select * from users where email = $1',[req.body.email], (err, row) => {
       if (err) throw err;
       if (row.rows.length ==  0) 
         return res.send('data tidak ditemukan')
       
-      // bcrypt.compare(req.body.password, row.rows[0].password, function(err, result) {
+      bcrypt.compare(req.body.password, row.rows[0].password, function(err, result) {
         
-        if (row) {
+        if (result) {
+          req.session.users = row.rows[0];
           res.redirect("/");
           
         }
+      });
     });
     })
 
+    router.get("/logout", function(req, res, next) {
+      req.session.destroy(function(err) {
+        res.redirect("/login");
+      })
+      
+    })
 
-  router.get("/", function (req, res, next) {
-    let sql = `select * from projects order by projectid`;
+
+  router.get("/", helpers.isLoggedIn, function (req, res, next) {
+    const { id, name } = req.query;
+    const url = req.url == '/' ? '/?page=1' : req.url;
     
+    
+    
+    let params = [];
+    if (name) {
+      params.push(`name ilike '%${name}%'`);
+    }
+    
+    if (id) {
+      params.push(`projectid = ${id}`);
+    }
+    let sql = `select * from projects `;
+    if (params.length > 0) {
+      sql += ` where ${params.join(" and ")}`;
+    }
+    console.log(sql)
     db.query(sql, (err, row) => {
       if (err) throw err;
 
       if (row) {
-        res.render("home/list", {nama: row.rows});
+        res.render("home/list", {nama: row.rows, query: req.query});
       }
     });
   });
 
-  router.get("/add", (req, res) => res.render("add"));
-  router.post("/add", (req, res) => {
+  router.get("/add", helpers.isLoggedIn, (req, res) => res.render("add"));
+  router.post("/add", helpers.isLoggedIn, (req, res) => {
     let sql = `INSERT INTO projects (projectid, name) VALUES ('${req.body.id}', '${req.body.name}')`;
    
     db.query(sql, (err) => {
@@ -80,28 +106,38 @@ console.log(sql)
     res.redirect("/");
   });
 
-  router.get("/profile", (req, res, next) => {
-    res.render("home/form");
+  router.get("/profile", helpers.isLoggedIn, (req, res, next) => {
+    let sql = `select * from users`;
+    console.log(sql);
+    db.query(sql, (err, row) => {
+      if (err) throw err;
+      
+
+      if (row) {
+        res.render("home/form", { nama: row.rows });
+      }
+ 
+  });
+    
   });
 
-  router.post("/profile:id", (req, res, next) => {
-    let sql = `UPDATE users SET email = '${req.body.email}', password = '${req.body.password}', type = '${req.body.type}'
-        WHERE projectid='${req.params.id}'`;
-        console.log(sql)
+  router.post("/profile", helpers.isLoggedIn, (req, res, next) => {
+    bcrypt.hash(`${req.body.password}`, saltRounds, function(err, hash) {
+    let sql = `UPDATE users SET email = '${req.body.email}', password = '${hash}', type = '${req.body.type}'
+        WHERE email = '${req.body.email}'`;
         db.query(sql, (err) => {
           if (err) throw err;
-        //   sql = `UPDATE projects 
-        // SET role = '${req.body.position}'`;
-        // if (req.body.position > 0) {
-        //   sql += `WHERE projectid='${req.params.id}'`
-        // }
-
-        // db.query(sql, (err) => {
-          // if (err) throw err;
+          sqll = `UPDATE members 
+          SET role = '${req.body.position}' WHERE email = '${req.body.email}''`
+          
+          
+          console.log(sql)
+        db.query(sql, sqll, (err) => {
+          if (err) throw err;
           res.redirect("/");
-        // });
+        });
       });
-
+    });
   });
 
   return router;
