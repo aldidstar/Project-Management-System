eaba45621719913bef11634a3471cc93e28eaaf4
Aldi Destaryana <aldidestaryana96@gmail.com>
2021-06-28 22:51:42 +0700

    option belommmm

--- routes/index.js
+++ routes/index.js
@@ -77,9 +77,9 @@ module.exports = function (db) {
       }
     });
   });
-  // router.post("/", helpers.isLoggedIn, (req, res) => {
-  //   let sql = `INSERT INTO users (projectid, projectname) VALUES ('${req.body.idpro}', '${req.body.namepro}')`;
-  // });
+  router.post("/project", helpers.isLoggedIn, (req, res) => {
+    let sql = `INSERT INTO users (projectid, projectname) VALUES ('${req.body.idpro}', '${req.body.namepro}')`;
+  });
 
   router.get("/add", helpers.isLoggedIn, (req, res) => res.render("add"));
   router.post("/add", helpers.isLoggedIn, (req, res) => {
