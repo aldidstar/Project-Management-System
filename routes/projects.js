34e2be0235b6836fadef3dd0760f7463f9285545
Aldi Destaryana <aldidestaryana96@gmail.com>
2021-07-07 23:19:25 +0700

    bingung delete

--- routes/projects.js
+++ routes/projects.js
@@ -111,7 +111,7 @@ module.exports = function (db) {
     db.query(sql, (err) => {
       if (err) throw err;
 
-      let sql = `INSERT INTO members (userid,role) VALUES ('${req.body.userid}', '${req.body.position}')`;
+      let sql = `INSERT INTO members (userid,role,projectid) VALUES ('${req.body.userid}', '${req.body.position}', '${req.body.id}')`;
 
       db.query(sql, (err) => {
         if (err) throw err;
@@ -122,8 +122,18 @@ module.exports = function (db) {
 
   router.get("/delete/:id", (req, res) => {
     let sql = `DELETE FROM projects  WHERE projectid=${req.params.id}`;
-    db.query(sql, (err) => {});
-    res.redirect("/");
+    console.log(sql)
+    db.query(sql, (err) => {
+      if (err) throw err;
+      
+      let sql = `DELETE FROM members  WHERE projectid=${req.params.id}`;
+      console.log(sql)
+      db.query(sql, (err) => {
+        if (err) throw err;
+    });
+      
+    });
+    res.redirect("/projects");
   });
 
   router.get("/edit/:id", (req, res) => {
@@ -131,12 +141,16 @@ module.exports = function (db) {
     console.log(sql);
     db.query(sql, (err, row) => {
       if (err) throw err;
+      let sql = `select * from users`;
 
+      db.query(sql, (err, memberss) => {
+        if (err) throw err;
       if (row) {
-        res.render("edit", { nama: row.rows[0] });
+        res.render("edit", { nama: row.rows[0],memberss: memberss.rows, });
       }
     });
   });
+});
 
   router.post("/edit/:id", (req, res) => {
     let sql = `UPDATE projects 
